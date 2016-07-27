77a4c433f809b24ca37178ebb9e61d359bbbed2b
Andrew E Wilson <lavatech.co@gmail.com>
2016-07-26 20:26:13 -0400

    seven segment core

---
+++ Projects/Seven_Segment/seven_segment_core.vhd
@@ -0,0 +1,45 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    20:25:59 07/26/2016 
+-- Design Name: 
+-- Module Name:    seven_segment_core - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity seven_segment_core is
+    Port ( Data_in : in  STD_LOGIC_VECTOR (31 downto 0);
+           Dp_in : in  STD_LOGIC_VECTOR (2 downto 0);
+           Seven_out : out  STD_LOGIC_VECTOR (6 downto 0);
+           Enable_out : out  STD_LOGIC_VECTOR (2 downto 0));
+end seven_segment_core;
+
+architecture Behavioral of seven_segment_core is
+
+begin
+
+
+end Behavioral;
+
